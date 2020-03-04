f175da651e21f49403de168a6235bb9dff7ac7be
adeilt <56762639+adeilt@users.noreply.github.com>
2020-03-04 05:32:14 -0800

    Update ExampleMod.java
    Change name of mod and remove a comment.

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,31 +1,28 @@
 package com.example.examplemod;
 
+import org.apache.logging.log4j.Logger;
+
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+public class ExampleMod {
+	public static final String MODID = "pristine-reaper";
+	public static final String NAME = "Pristine Reaper";
+	public static final String VERSION = "0.1";
 
-    private static Logger logger;
+	private static Logger logger;
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
-    }
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		logger = event.getModLog();
+	}
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+	}
 }
